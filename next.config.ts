2c5b3ca6431ecaa5661bb53ce2a8ca47ab82db5f
Marvin_Rop <marwin5411@outlook.com>
2025-12-22 11:27:55 +0700

    config: enable trailing slashes in Next.js output configuration

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin('./app/i18n/request.ts');
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true,
     remotePatterns: [
